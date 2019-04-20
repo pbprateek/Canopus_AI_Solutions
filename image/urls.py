b38774abe458af010336e6faeaaac7bb034426e9
Soham Das <soham5959595@gmail.com>
2019-04-21 04:35:17 +0530

    fadeIn animation added to all components. All features divided into separate routes. Services layout changed. Deep Dreams functionality working. Form design changed. Background changed. Sidebar changed. Sample vector added to background. Need better vector. Need to connect other 3 feature functionalities to backend. Need to add content in Dashboard. Need to add text content in Services layout bubbles.

--- image/urls.py
+++ image/urls.py
@@ -6,12 +6,13 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.home, name='index'),
     url(r'return-weights/', views.calculateWeights, name='scrape'),
-    url(r'img-class/', views.imgClass, name='img'),
-    url(r'deep-dreams/', views.deepDreams, name='dream'),
-    url(r'style-trans/', views.styleTransfer, name='style'),
+    url(r'img-classification/', views.imgClass, name='imgClass'),
+    url(r'deep-dreams/', views.deepDreams, name='deepDreams'),
+    url(r'style-transfer/', views.styleTransfer, name='styleTransfer'),
     url(r'imageUpload/', views.imageUpload, name='imageupload'),
     url(r'styleUpload/', views.styleUpload, name='styleupload'),
-    url(r'services/', views.services, name='services'),
+    url(r'face-recognition/', views.faceRecognition, name='faceRecognition'),
+    url(r'services/', views.services, name='services')
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
