9c42805954b68069336d7097d035f575f22ae133
prateek_kumar <prateek2109@outlook.com>
2019-04-26 12:04:42 +0530

    Minor Fix

--- apiTool/urls.py
+++ apiTool/urls.py
@@ -6,5 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'colorit/', views.colorizeAPI, name='colorizeAPI'),
     url(r'recognizeface/', views.faceRecogApi, name='faceRecogAPI'),
-    url(r'simply/', views.simple, name='faceRecogAPI')
 ]
